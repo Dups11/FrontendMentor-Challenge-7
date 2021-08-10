adb6a5623acaa86b6046c6920379bb3725bbed0a
Dups <dupuis.dav.code@gmail.com>
2021-08-10 18:06:16 +0200

    Js

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ document.getElementById("input").addEventListener("change", foo);
 function foo() {
     let bill = document.getElementById("bill").value;
     let people = document.getElementById("people").value;
+    let customValue = document.getElementById("customValue").value;
     const form = document.forms[0];
     let tipValue = form.elements["tip"];
-    let total = (bill * parseInt(tipValue.value)) / 100;
+    let total = 0
+
+    if (customValue > 0) {
+        total = ((bill * customValue) / 100).toFixed(2);
+    } else {
+        // tipValue = form.elements["tip"];
+        total = (bill * parseInt(tipValue.value)) / 100;
+    }
     if (people > 0 && bill > 0) {
         document.getElementById("total").innerHTML = "$" + total;
         let amount = (total / people).toFixed(2);
         document.getElementById("amount").innerHTML = "$" + amount;
     }
-
+    console.log(tipValue.value);
 }
 
-document.getElementById('custom').addEventListener('click', customTip)
 
-function customTip() {
-    let customValue = document.getElementById('custom').value
-    document.getElementById('custom').innerHTML = "0"
-}
 
 document.getElementById("reset").addEventListener("click", reset);
-
 
 function reset() {
     document.getElementById("total").innerHTML = "$0.00";
